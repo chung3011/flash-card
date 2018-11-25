691c400ce501ea03a2aa5a96b561a319a929840a
dunghgt <dangtothedung@gmail.com>
2018-11-25 11:03:13 +0700

    load

--- android/app/src/main/java/com/flashcard/MainApplication.java
+++ android/app/src/main/java/com/flashcard/MainApplication.java
@@ -3,6 +3,8 @@ package com.flashcard;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import io.invertase.firebase.RNFirebasePackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import io.invertase.firebase.RNFirebasePackage;
 import io.invertase.firebase.auth.RNFirebaseAuthPackage;
@@ -27,6 +29,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+            new RNFirebasePackage(),
             new VectorIconsPackage(),
           new RNFirebasePackage(),
           new RNFirebaseAuthPackage(),
